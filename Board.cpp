240b653b019ee8b67b34cbb6991782ed2b0890b2
NicholasReis <NicholasCReis@gmail.com>
2022-01-06 14:23:10 -0800

    Readability update
    Changed the layout to be more readable. Also shifted values to more clearly distinguish the numbers from 0. 8 and 6 look too similar so they are now 2s, 3s, 7s, 5s, etc. Display board also correctly displays [x][y] array.

--- Board.cpp
+++ Board.cpp
@@ -4,28 +4,31 @@ void displayBoard(int[9][9]);
 
 int main()
 {
-    int board[9][9] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+    int board[9][9] = {{0, 2, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 3, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 1, 0, 0, 0, 0, 0},
                      {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 4, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 6, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 7, 0, 0},
                      {0, 0, 0, 0, 0, 0, 0, 0, 0},
                      {0, 0, 0, 0, 0, 0, 0, 0, 0},
                      {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 1, 0, 0, 0, 0, 0, 0}};
+                     {0, 0, 5, 0, 0, 0, 0, 0, 0}};
 
     displayBoard(board);
 
     return 0;
 }
 
+
+//Displays the board when given the array
 void displayBoard(int board[9][9])
 {
     for (int x = 0; x < 9; x++)
     {
         for (int y = 0; y < 9; y++)
         {
-            //Been a little while may be [x][y] or [y][x]
+            //Formats the board with a single line dividing cells
+            //And a double to divide quadrants (or whatever they are called)
             std::cout << board[x][y];
             if ((y+1) % 3 == 0 && y!= 8)
             {
@@ -33,10 +36,13 @@ void displayBoard(int board[9][9])
             }else if(y != 8){
                 std::cout << " | ";
             }
+        }//Adds a second line to divide quadrants
+        if((x+1) % 3 ==0){
+            std::cout << std::endl <<  "----------||-----------||----------";
+            std::cout << std::endl <<  "----------||-----------||----------";
+        }else if(x != 8){
         }
-        if((x+1)%3 ==0 && x != 8){
-            std::cout << std::endl <<  "___________________________________";
-        }
+        
         std::cout<< std::endl;
     }
 }
