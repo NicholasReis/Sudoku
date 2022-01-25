c4250f6cd00eea74163e6ad51a3a8c211a419a9d
NicholasReis <NicholasCReis@gmail.com>
2022-01-24 17:49:04 -0800

    Branch Template Setup
    The necessary files and boilerplate functions for this branch.

---
+++ Sudoku_Solver.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+//Function Declarations
+
+
+//Checks if the game is solveable
+bool solveable(std::vector<std::vector<int>> board){
+
+    return false;
+}
+
+
+//Checks if the game can be solved in exactly one way
+bool properSudoku(std::vector<std::vector<int>> board){
+
+    return false;
+}
---
+++ Sudoku_Solver.h
@@ -0,0 +1,5 @@
+#ifndef SUDOKU_SOLVER_H
+#define SUDOKU_SOLVER_H
+bool solveable(std::vector<std::vector<int>>);
+bool properSudoku(std::vector<std::vector<int>>);
+#endif
